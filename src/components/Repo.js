66977c6898fefbad29ed3a8672e522849cc7f43e
holdenc20 <kcholden@me.com>
2023-08-14 12:13:52 -0400

    adding proj repos - with image processing

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,15 +1,45 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Repo = ({ repo }) => {
   const [readmeContent, setReadmeContent] = useState('');
+  const [imageUrls, setImageUrls] = useState([]);
 
   // Fetch README content for the repository
   const fetchReadmeContent = async () => {
     try {
       const response = await axios.get(repo.contents_url.replace('{+path}', 'README.md'));
-      const content = atob(response.data.content); // Decode Base64 content
-      setReadmeContent(content);
+      const content = atob(response.data.content);
+
+      // Check if the content has HTML tags or not
+      const isHtml = content.trim().startsWith('<');
+
+      // If content has HTML tags, render as HTML, otherwise render as plain text
+      if (isHtml) {
+        const absoluteContent = content.replace(/src="([^"]*)"/g, (match, src) => {
+          if (src.startsWith('http') || src.startsWith('/')) {
+            return match;
+          } else {
+            return `src="${repo.html_url}/raw/main/${src}"`;
+          }
+        });
+
+        setReadmeContent(absoluteContent);
+      } else {
+        // Extract image URLs from the content using a regex pattern
+        const imageMatches = content.match(/!\[[^\]]*\]\((https:\/\/user-images\.githubusercontent\.com\/[^)]+)\)/g);
+        const extractedImageUrls = imageMatches
+          ? imageMatches.map(match => match.match(/\((https:\/\/user-images\.githubusercontent\.com\/[^)]+)\)/)[1])
+          : [];
+
+        // Replace relative image URLs with absolute URLs
+        const absoluteContent = content.replace(/!\[([^\]]*)\]\((https:\/\/user-images\.githubusercontent\.com\/[^)]+)\)/g, (match, altText, src) => {
+          return `![${altText}](${src})`;
+        });
+
+        setReadmeContent(absoluteContent);
+        setImageUrls(extractedImageUrls);
+      }
     } catch (error) {
       console.error('Error fetching README content:', error);
     }
@@ -26,7 +56,14 @@ const Repo = ({ repo }) => {
       <a href={repo.html_url}>View on GitHub</a>
       <div>
         <h4>README Content:</h4>
-        <pre>{readmeContent}</pre>
+        {imageUrls.map((imageUrl, index) => (
+          <img key={index} src={imageUrl} alt={`Image ${index}`} />
+        ))}
+        {readmeContent.startsWith('<') ? (
+          <div dangerouslySetInnerHTML={{ __html: readmeContent }} />
+        ) : (
+          <pre>{readmeContent}</pre>
+        )}
       </div>
     </div>
   );
